725f696bc2777eecce96859073f22e351f067ccd
Nekeroo <77273805+Nekeroo@users.noreply.github.com>
2025-05-20 10:05:45 +0200

    Init Project

---
+++ src/main/java/com/project/vestiart/VestiArtApplication.java
@@ -0,0 +1,13 @@
+package com.project.vestiart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VestiArtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VestiArtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/vestiart/VestiArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.vestiart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VestiArtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
