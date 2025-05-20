626e1ade67ccf8992d8abb4beb3beb7ee860c824
Hyuga974 <tamashiyami22@gmail.com>
2025-05-20 14:03:14 +0200

    Test: OpenAI connection in progress

--- src/main/java/com/project/vestiart/openai/config/OpenAIConfig.java
+++ src/main/java/com/project/vestiart/openai/config/OpenAIConfig.java
@@ -1,4 +1,23 @@
 package com.project.vestiart.openai.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
 public class OpenAIConfig {
+    @Bean
+    @Qualifier("openAiWebClient")
+    public WebClient openAiWebClient(@Value("${spring.ai.openai.api-key}") String apiKey) {
+        return WebClient.builder()
+                .baseUrl("https://api.openai.com")
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + apiKey)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
 }
