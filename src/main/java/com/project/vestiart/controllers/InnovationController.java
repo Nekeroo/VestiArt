aff86821ef5a9cac81a75f5dfaedcce589b51756
Nekeroo <77273805+Nekeroo@users.noreply.github.com>
2025-05-22 23:30:23 +0200

    feat: Multiple RequestInput at once

--- src/main/java/com/project/vestiart/controllers/InnovationController.java
+++ src/main/java/com/project/vestiart/controllers/InnovationController.java
@@ -12,6 +12,10 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Base64;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 @RequestMapping("/innovation")
@@ -37,6 +41,30 @@ public class InnovationController {
                 .description(resultFromTheIdea)
                 .title(input.getPerson() + " Collection")
                 .build();
+    }
+
+    @PostMapping("/create/multiple")
+    public List<IdeaDTO> createMultipleIdeasFromRequest(@RequestBody List<RequestInput> inputs) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(Math.min(inputs.size(), 10));
+
+        List<CompletableFuture<IdeaDTO>> futures = inputs.stream()
+                .map(input -> CompletableFuture.supplyAsync(() -> {
+                    try {
+                        return createIdeaFromRequest(input);
+                    } catch (IOException | URISyntaxException e) {
+                        throw new RuntimeException(e);
+                    }
+                }, executorService))
+                .toList();
+
+        List<IdeaDTO> ideaDTOS = futures.stream()
+                .map(CompletableFuture::join)
+                .toList();
+
+        executorService.shutdown();
+
+        return ideaDTOS;
     }
 
 }
