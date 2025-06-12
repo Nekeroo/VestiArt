a8130856b12c478d4f98326049506a3b451217ae
Nekeroo <77273805+Nekeroo@users.noreply.github.com>
2025-06-12 11:14:47 +0200

    ajout message

---
+++ src/main/java/com/project/vestiart/models/ErrorMessage.java
@@ -0,0 +1,14 @@
+package com.project.vestiart.models;
+
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorMessage {
+
+    private String message;
+
+}
