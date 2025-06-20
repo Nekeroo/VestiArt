809a0399cb1d7d43065aeb1b69ba7243ea5002a8
Nekeroo <77273805+Nekeroo@users.noreply.github.com>
2025-06-20 21:45:50 +0200

    Ajout TODO sur main

--- src/main/java/com/project/vestiart/VestiArtApplication.java
+++ src/main/java/com/project/vestiart/VestiArtApplication.java
@@ -10,4 +10,13 @@ public class VestiArtApplication {
         SpringApplication.run(VestiArtApplication.class, args);
     }
 
+    // TODO : User - Récupérer ses propres créations
+    // TODO : User - Ajouter un système de like (PS : Ajouter une table de jointure id_user, id_idea)
+    // TODO : Admin - éditer un user
+    // TODO : Admin - Avoir plus de contrôles sur les créations
+    // TODO : Admin - Possibilité de HIDE une Idea (eventuellement directement la supprimer ? )
+    // TODO : Admin - Lister les utilisateurs
+    // TODO : Admin - Ban user
+
+
 }
